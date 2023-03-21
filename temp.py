24b720aa70bdd32aa2c78c18f74595b668d031e8
Pedro Augusto Cabral Oliveira <104571614+PedroAugusto2004@users.noreply.github.com>
2023-03-21 17:32:01 -0300

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,8 @@
+temp_celsius= float(input("Digite aqui a tempertura em graus Celsius"))
+
+temp_fahrenheit = temp_celsius * 1.8 + 32
+
+print("A temperatura de graus Fahrenheitt é: ", temp_fahrenheit)
+
+if temp_fahrenheit > 100:
+    print("Está muito quente!")
